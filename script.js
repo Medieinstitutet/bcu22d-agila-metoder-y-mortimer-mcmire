9579f378aac80de8dce6ad003f76d7552571aca2
EMELIE RIBRING <emelieribring@hotmail.com>
2023-01-03 22:01:07 +0100

    metamask

--- script.js
+++ script.js
@@ -24,3 +24,25 @@ fredrik.addEventListener("click", ()=>{
 homeBtn.addEventListener("click", ()=>{
   document.location.reload()
 })
+
+
+//Sign in meta mask 
+document.getElementById('metaBtn').addEventListener('click', event => {
+    let account;
+    let button = event.target;
+    ethereum.request({method: 'eth_requestAccounts'}).then(accounts => {
+        account = accounts[0];
+        console.log("connected account", account);
+        button.textContent = account;
+
+        ethereum.request({method: 'eth_getBalance', params: [account, 'latest']}).then(result => {
+            console.log("result", result);
+            let wei = parseInt(result,16);
+            let balance = wei / (10**18);
+            console.log("Balance in ETH", balance);
+            let balanceElement = document.createElement("p");
+            document.body.appendChild(balanceElement);
+            balanceElement.innerHTML = 'Your balance ETH-balance is: ' + balance.toFixed(4);
+        });
+    });
+});
