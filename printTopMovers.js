cf281e747c0f7509cc7721973e98a4006be8235c
FredKarlsson <fredrik.karlsson@medieinstitutet.se>
2023-01-08 21:17:55 +0100

    removed comments

--- printTopMovers.js
+++ printTopMovers.js
@@ -7,28 +7,23 @@ export default async function printTopMovers() {
     const sortedListAscending = await getSortedList();
     sortedListAscending.sort((a, b) => a.price_change_percentage_24h - b.price_change_percentage_24h);
     const bottomFive = sortedListAscending.slice(0, 5);
-    console.log("Asc:", bottomFive);
 
     const bottomMoversList = document.createElement('ul');
     content.appendChild(bottomMoversList);
-    console.log("123");
 
     bottomFive.forEach((item) => {
         const li = document.createElement('li');
         li.innerHTML = `${item.id}: ${item.price_change_percentage_24h}%`;
         bottomMoversList.appendChild(li);
     });
-    console.log("Du är korkad bror");
 
     const sortedListDescending = await getSortedList();
     sortedListDescending.sort((a, b) => b.price_change_percentage_24h - a.price_change_percentage_24h);
     const topFive = sortedListDescending.slice(0, 5);
-    console.log("Desc:", topFive);
 
 
     const topMoversList = document.createElement('ul');
     content.appendChild(topMoversList);
-    console.log("678");
 
     topFive.forEach((item) => {
         const li = document.createElement('li');
